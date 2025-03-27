09587149d0c4f0154f003a567290bc3e3aaf59bd
Evgenii <evgen.ak24@gmail.com>
2025-03-27 17:57:31 +0500

    prefinal

--- task_manager/tests/conftest.py
+++ task_manager/tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
+from django.core.management import call_command
 
 @pytest.fixture(scope='session')
 def django_db_setup(django_db_blocker):
-    from django.core.management import call_command
     with django_db_blocker.unblock():
         call_command('migrate')
