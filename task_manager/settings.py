5ddcab7b92b2ca6f040a4f4821b1bf7a9ae9151f
Evgenii <evgen.ak24@gmail.com>
2025-03-27 16:39:41 +0500

    prefinal

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -106,11 +106,12 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
+
 
 if os.getenv('DATABASE_URL'):
     db_from_env = dj_database_url.config(conn_max_age=600)
